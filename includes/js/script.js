fabc4eabf63d9f81208ca7e4df74653e32446166
zunaid <zunaidq07@gmail.com>
2018-02-15 12:37:28 +0530

    first push

---
+++ includes/js/script.js
@@ -0,0 +1,15 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+$(document).ready(function(){
+	$('.carousel').carousel({
+		interval:2000
+	})
+});
